7525980c745f9710ddd415db966f454166e91326
NatalieBlack <natalievictoriablack@gmail.com>
2015-04-25 14:18:44 -0400

    processing diptych

--- source/javascripts/util.js
+++ source/javascripts/util.js
@@ -1,3 +1,23 @@
 function consoleLog(message) {
         console.log(message);
 } 
+//$('canvas').width(jQuery(window).width() * 0.5);
+//console.log($('canvas').offsetWidth);
+//$('canvas').height(setupHeight);
+function makeDiptych() {
+    console.log('hi');
+   w = $('.diptych').width(); 
+   $('.diptychPanel').width(w * 0.4); 
+}
+window.addEventListener('load', function () {
+  makeDiptych();
+}, false);
+$(window).load(function() {
+    makeDiptych();
+});
+$(window).resize(function(){
+    makeDiptych();
+});
+$(window).scroll(function() {
+    makeDiptych();
+});

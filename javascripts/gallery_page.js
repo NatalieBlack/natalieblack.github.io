89fdb1d9caa39943c44befd06c6e7b13c7cfcc09
NatalieBlack <natalievictoriablack@gmail.com>
2019-04-27 22:17:27 -0400

    Add JS to check if processing canvas will fit in screen width on load and on window resize. Display warning if screen is too small

--- javascripts/gallery_page.js
+++ javascripts/gallery_page.js
@@ -1,4 +1,13 @@
-$(document).ready(function(){
+(document).ready(function(){
+    var canvas = document.querySelector('div.gallery_page canvas');
+    var canvasDiv = document.querySelector('div.gallery_page');
+    var canvasWidth = canvas.offsetWidth;
+    var p = document.createElement('p');
+    document.body.prepend(p);
+    p.id = 'warning';
+    p.style.display = 'none';
+    p.innerText = "This piece is too large to view on your device";
+
     $('.gallery_page').slick({
         dots: true,
         fade: true,
@@ -8,4 +17,21 @@ $(document).ready(function(){
         swipe: false,
         focusOnSelect: false
     });
+
+    showOrHideArt(); 
+
+    window.onresize = function(e) {
+      showOrHideArt();
+    };
+
+    function showOrHideArt() {
+        if(window.innerWidth <= canvasWidth){
+          canvasDiv.style.display = 'none';
+          p.style.display = 'block';
+        } else {
+          canvasDiv.style.display = '';
+          p.style.display = 'none';
+        }
+    }   
 });
+

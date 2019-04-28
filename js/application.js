41d69ebdb558e2b9e679611ed4acc8fc66c51960
NatalieBlack <natalievictoriablack@gmail.com>
2019-04-27 23:06:18 -0400

    use id instead of class to select div containing canvas

--- js/application.js
+++ js/application.js
@@ -14,8 +14,8 @@ $(window).on('load', function() {
 
     var galleryPage = document.querySelector('body#gallery_page')
     if(galleryPage) {
-      var canvas = document.querySelector('canvas');
-      var canvasDiv = document.querySelector('div.gallery_page');
+      var canvas = document.querySelector('div#gallery_page canvas');
+      var canvasDiv = document.querySelector('div#gallery_page');
       var canvasWidth = canvas.offsetWidth;
       var p = document.createElement('p');
       document.body.prepend(p);
